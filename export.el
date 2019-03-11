822c1bb8dd87b1545784f8322b626c089975a174
rgb-24bit <rgb-24bit@foxmail.com>
2019-03-11 21:01:55 +0800

    blog: 导出时使用样式文件的样式而非内联样式

--- export.el
+++ export.el
@@ -63,6 +63,7 @@
 
   ;; HTML Specific export settings
   (setq org-html-doctype "html5")
+  (setq org-html-htmlize-output-type 'css)
   (setq org-html-link-home "https://rgb-24bit.github.io")
   (setq org-html-link-up "https://rgb-24bit.github.io/blog/")
   (setq org-html-head (read-file-text "misc/html-head.html")))
