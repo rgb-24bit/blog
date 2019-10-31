9aaabb892a1d7e260b866df3223f9d9f1a0840ca
rgb-24bit <rgb-24bit@foxmail.com>
2019-10-31 11:28:25 +0800

    blog: 更新导出脚本

--- export.el
+++ export.el
@@ -42,6 +42,19 @@
     (insert-file-contents-literally file-name)
     (decode-coding-region (point-min) (point-max) 'utf-8 t)))
 
+;; Refrence https://coldnew.github.io/a1ed40e3/
+(defadvice org-html-paragraph (before org-html-paragraph-advice
+                                      (paragraph contents info) activate)
+  "Join consecutive Chinese lines into a single long line without
+unwanted space when exporting org-mode to html."
+  (let* ((origin-contents (ad-get-arg 1))
+         (fix-regexp "[[:multibyte:]]")
+         (fixed-contents
+          (replace-regexp-in-string
+           (concat
+            "\\(" fix-regexp "\\) *\n *\\(" fix-regexp "\\)") "\\1\\2" origin-contents)))
+    (ad-set-arg 1 fixed-contents)))
+
 (defun init-export-env ()
   ;; Dependency
   (load-file "htmlize.el")
